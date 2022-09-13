704317b967267da1c62bac8dd16de135af2e609f
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2022-09-13 18:49:42 +0530

    first commit

---
+++ ottanime/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+# Create your models here.
+
+AGE_CHOICES = {
+    ('All', 'All'),
+    ('kids', 'kids'),
+}
+
+MOVIE_CHOICES = {
+    ('seasonal', 'Seasonal'),
+    ('single', 'Single'),
+}
+
+
+class CustomUser(AbstractUser):
+    profiles = models.ManyToManyField(
+        'Profile', blank=True)
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=1000)
+    age_limit = models.CharField(choices=AGE_CHOICES, max_length=10)
+    # keep track of individual user id
+    uuid = models.UUIDField(default=uuid.uuid4)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # keep track of individual user id
+    uuid = models.UUIDField(default=uuid.uuid4)
+    type = models.CharField(choices=MOVIE_CHOICES, max_length=100)
+    video = models.ManyToManyField('Video')
+    image = models.ImageField(upload_to="covers")
+    age_limit = models.CharField(choices=AGE_CHOICES, max_length=10)
+
+    def __str__(self):
+        return self.title
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=1000)
+    file = models.FileField(upload_to="movies")
+
+    def __str__(self):
+        return self.title
---
+++ ottanime/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . views import *
+
+# here are my views
+
+app_name = 'ottanime'
+
+
+urlpatterns = [
+    path("", Home.as_view(), name="Home"),
+    path('profiles/', ProfileList.as_view(), name="profile-list"),
+    path('profiles/create/', ProfileCreate.as_view(), name="profile-create"),
+    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie-list"),
+    path('watch/detail/<str:movie_id>/',
+         MovieDetail.as_view(), name="movie-detail"),
+    path('watch/play/<str:movie_id>/', PlayMovie.as_view(), name="play-movie"),
+]
