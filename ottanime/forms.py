f6569f9eac200c9a10cb8f5a741add9d47e88d4b
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2023-01-18 04:17:07 +0530

    commits

--- ottanime/forms.py
+++ ottanime/forms.py
@@ -3,6 +3,9 @@ from . models import Profile
 
 class ProfileForm(ModelForm):
     class Meta:
+        # adding profile from the model to the model
         model = Profile
         # excluding the uuid field
         exclude = ['uuid']
+
+        # so in this we are asking every things form profile model but excluding uuid 
--- ottanime/views.py
+++ ottanime/views.py
@@ -3,23 +3,25 @@ from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
+# importing models and form .py files 
 from . forms import *
 from . models import *
 
 
 class Home(View):
     def get(self, request, *args, **kwargs):
+        # if user is authenticated than send him to create the profile page 
         if request.user.is_authenticated:
             return redirect('ottanime:profile-list')
+        # otherwise send him to the index page
         return render(request, 'index.html')
 
 
+# user need to be authenticated
 method_decorator(login_required, name='dispatch')
-
-
 class ProfileList(View):
     def get(self, request, *args, **kwargs):
-        # it gets the logged in user (request.user)
+        # it gets the logged in user (request.user) and all the user profiles
         profiles = request.user.profiles.all()
         # passing temp to context of profile list
         context = {
@@ -28,9 +30,8 @@ class ProfileList(View):
         return render(request, 'profilelist.html', context)
 
 
+
 method_decorator(login_required, name='dispatch')
-
-
 class ProfileCreate(View):
     # get method decorator
     def get(self, request, *args, **kwargs):
@@ -42,14 +43,16 @@ class ProfileCreate(View):
 
     # post method decorator
     def post(self, request, *args, **kwargs):
+        # request.post to get info that user has passed in post 
         form = ProfileForm(request.POST or None)
         #
         if form.is_valid():
-            # creating the profile form if is_valid usr
+            # creating the profile form if is_valid usr or info provided is in our constraints
+            # **form.is_valid is used in place of user = user and name = name works the same way
             profile = Profile.objects.create(**form.cleaned_data)
             # checking if usr has profile
             if profile:
-                # if exixt create a option to add another profile
+                # if user is logged then add this profile
                 request.user.profiles.add(profile)
                 return redirect('ottanime:profile-list')
         context = {
@@ -59,12 +62,10 @@ class ProfileCreate(View):
 
 
 method_decorator(login_required, name='dispatch')
-
-
 class MovieList(View):
     def get(self, request, profile_id, *args, **kwargs):
         try:
-            #getting obj of profile in profile
+            #getting obj of profile in profile of a particular user here uuid is profile_id
             profile = Profile.objects.get(uuid=profile_id)
             #getting the movies & filtering the movies with age limit
             movies = Movie.objects.filter(age_limit=profile.age_limit)
@@ -80,13 +81,12 @@ class MovieList(View):
             return redirect('ottanime:profile-list')
 
 
+#you can also use @login_required insted of method decorator to check login when you use def based view
 method_decorator(login_required, name='dispatch')
-
-
 class MovieDetail(View):
     def get(self, request, movie_id, *args, **kwargs):
         try:
-            #getting movire acc. to uuid from request usr
+            #getting movire acc. to uuid from request usr to movie_id
             movie = Movie.objects.get(uuid=movie_id)
 
             context = {
@@ -99,8 +99,6 @@ class MovieDetail(View):
 
 
 method_decorator(login_required, name='dispatch')
-
-
 class PlayMovie(View):
     def get(self, request, movie_id, *args, **kwargs):
         try:
